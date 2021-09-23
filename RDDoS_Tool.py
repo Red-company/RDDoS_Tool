d9a161a24ad223202b7bbd27ab79556d58928ffc
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-09-24 00:24:57 +0300

    About page & start from 1 port(v1.1)

--- RDDoS_Tool.py
+++ RDDoS_Tool.py
@@ -6,7 +6,7 @@ import random
 import socket
 import pyfiglet
 
-version = "1.0"
+version = "1.1"
 
 
 #platform info
@@ -36,7 +36,7 @@ while True:
     print("       Github: https://github.com/Red-company/RDDoS_Tool")
     print('                   For legal purposes only')
     print("\033[92;1m")
-    print("1. Website Domain\n2. IP Addresse\n3. Exit")
+    print("1. Website Domain\n2. IP Addresse\n3. About\n4. Exit")
     print('\033[0m')
 
     opt = str(input("\n> "))
@@ -51,6 +51,29 @@ while True:
         break
 
     elif opt == '3':
+        print("\n\033[101m                \033[0m  \033[101m       \033[0m  \033[101m        \033[0m \033[101m       \033[0m \033[0m                \033[92m_____\033[0m")
+        print("                  \033[101m   \033[0m  \033[101m   \033[0m \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m\033[0m             \033[92m.-'     '-.\033[0m")
+        print("\033[101m                \033[0m  \033[101m       \033[0m  \033[101m      \033[0m   \033[101m   \033[0m  \033[101m   \033[0m           \033[92m.'\033[91m____\033[0m secure\033[92m'.\033[0m")
+        print("                  \033[101m   \033[0m \033[101m   \033[0m  \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m          \033[92m/  \033[91m|  _ \\\033[0m  \033[93m__\033[0m   \033[92m\\\033[0m")
+        print("\033[101m                \033[0m  \033[101m   \033[0m   \033[101m   \033[0m \033[101m        \033[0m \033[101m       \033[0m         \033[92m;\033[0m r \033[91m| |_) /\033[0m\033[93m/ o\\\033[0m t \033[92m;\033[0m")
+        print("                                                     \033[92m|\033[0m e \033[91m|  _ <\033[0m \033[93m\\__/\033[0m e \033[92m|\033[0m")
+        print("RedDDoS Tool is an open source tool for              \033[92m;\033[0m d \033[91m|_| \\ \\\033[0m \033[93m<|\033[0m  a \033[92m;\033[0m")
+        print("penetration. You can test networks/servers/any        \033[92m\\       \033[91m\\/\033[0m \033[93m<|\033[0m  m\033[92m/\033[0m")
+        print("other devices with it.                                 \033[92m'.\033[0m member \033[93m<|\033[0m \033[92m.'\033[0m")
+        print("                                                         \033[92m'-._____.-'\033[0m")
+        print("Author of the program is not responsible for")
+        print("it's usage, everybody MUST use it ONLY in         member-id: 'rst-00000002'")
+        print("legit cases.")
+        
+        goon = input("\n\n\n\n\n\n\n\n\nPress Enter to continue.")
+
+        if uname == "Windows":
+            cmd = 'cls'
+        else:
+            cmd = 'clear'
+        os.system(cmd)
+
+    elif opt == '4':
         exit()
 
     else:
@@ -58,7 +81,7 @@ while True:
         time.sleep(2)
         os.system(cmd)
 
-port = int(input("Start port: "))
+port = 1
 os.system(cmd)
 print('\033[36;2mINITIALIZING....')
 time.sleep(1)
