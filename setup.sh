11bb85098f19654f89e863a050e9088440412acb
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-13 22:27:42 +0300

    Editing setup.sh 2

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ python3 -m pip install --upgrade pip > /dev/null 2>&1
 pip install tqdm > /dev/null 2>&1
 pip install pyfiglet==0.7 > /dev/null 2>&1
 clear
-echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mDDoS Tool is installed. To run it type '\033[1;4mpython3 RDDoS_Tool.py\033[0m' "
+echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mDDoS Tool was installed successfully."
+echo -e "\n\nTo run it type '\033[1;4mpython3 RDDoS_Tool.py\033[0m'\n\n"
