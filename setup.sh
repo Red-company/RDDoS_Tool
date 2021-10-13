f3d00b0d6d180b2d3df4d35a1e6c845b82b9fc27
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-13 18:00:28 +0300

    Editing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "                \e[91mInstalling \e[91mRed\e[0mDDoS Tool,"
+echo -e "                Installing \033[91mRed\033[0mDDoS Tool,"
 echo -e "                Please Wait..."
 apt-get -y install python-pip > /dev/null 2>&1
 apt-get -y install python3-pip > /dev/null 2>&1
@@ -8,4 +8,4 @@ python3 -m pip install --upgrade pip > /dev/null 2>&1
 pip install tqdm > /dev/null 2>&1
 pip install pyfiglet==0.7 > /dev/null 2>&1
 clear
-echo -e "\e[92m[*]\e[0m \e[91mRed\e[0mDDoS Tool is installed. To run \e[91mR\e[0mDDoS type '\e[1;4mpython3 RDDoS_Tool.py\e[0m' "
+echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mDDoS Tool is installed. To run it type '\033[1;4mpython3 RDDoS_Tool.py\033[0m' "
